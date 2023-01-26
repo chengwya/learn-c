08f9be72da4a29e64200e7b7a684314107043f80
chengwya <122613859+chengwya@users.noreply.github.com>
2023-01-26 23:29:59 +0800

    Add files via upload

---
+++ test_1_26/test_1_26/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//void print()
+//{
+//	printf("Hello World!\n");
+//}
+//
+//int main()
+//{
+//	print();
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr[] = "Hello World!";
+//	printf("%d", strlen(arr));
+//	return 0;
+//	
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	printf("%d", printf("%d", printf("%d", 43))); //可以想想结果是什么
+	return 0;
+}
