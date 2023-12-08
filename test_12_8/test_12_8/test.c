5f8d307d2d4123f16873b153f3a38ab0c4249471
子期 <122613859+chengwya@users.noreply.github.com>
2023-12-08 13:40:40 +0800

    Add files via upload

---
+++ test_12_8/test_12_8/test.c
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int i = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < n-1-i ; j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0; j <= i; j++)
+//			{
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (~scanf("%d", &n))
+//	{
+//		int i = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < i; j++)
+//			{
+//				printf(" ");
+//			}
+//
+//			for (j = 0; j < n-i; j++)
+//			{
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int i = 0;
+//		for (i = 0; i <= n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < n - i; j++)
+//			{
+//				printf(" ");
+//			}
+//			for (j = 0; j <= i; j++)
+//			{
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j <= i; j++)
+//			{
+//				printf(" ");
+//			}
+//			for (j = 0; j < n - i; j++)
+//			{
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}	
+//}
+
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	while (scanf("%d", &n) != EOF)
+	{
+		int i = 0;
+		for (i = 0; i <= n; i++)
+		{
+			int j = 0;
+			for (j = 0; j <= n-i ; j++)
+			{
+				printf("* ");
+			}
+			printf("\n");
+		}
+		for (i = 0; i < n; i++)
+		{
+			int j = 0;
+			for (j = 0; j <= i+1 ; j++)
+			{
+				printf("* ");
+			}
+			printf("\n");
+		}
+	}
+}
