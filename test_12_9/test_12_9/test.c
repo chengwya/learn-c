d15698d3b36b3dc389b4bfb84c033358256079d3
子期 <122613859+chengwya@users.noreply.github.com>
2023-12-09 12:04:13 +0800

    Add files via upload

---
+++ test_12_9/test_12_9/test.c
@@ -0,0 +1,159 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int i = 0;
+//		for (i = n; i >= 0; i--)
+//		{
+//			int j = 0;
+//			for (j = 0; j < i; j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0; j <= n-i; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j <= i; j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0; j < n-i; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//	}
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (~scanf("%d", &n))
+//	{
+//		int i = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < n; j++)
+//			{
+//				if (i == j)
+//				{
+//					printf("*");
+//				}
+//				else
+//				{
+//					printf(" ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (~scanf("%d", &n))
+//	{
+//		int i = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < n; j++)
+//			{
+//				if (i + j == n - 1)
+//				{
+//					printf("*");
+//				}
+//				else
+//				{
+//					printf(" ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (~scanf("%d", &n))
+//	{
+//		int i = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < n; j++)
+//			{
+//				if (i + j == n - 1)
+//				{
+//					printf("*");
+//				}
+//				else if (i == j)
+//				{
+//					printf("*");
+//				}
+//				else
+//				{
+//					printf(" ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	while (~scanf("%d", &n))
+	{
+		int i = 0;
+		for (i = 0; i < n; i++)
+		{
+			int j = 0;
+			for (j = 0; j < n; j++)
+			{
+				if (i == 0 || i == n - 1)
+				{
+					printf("* ");
+				}
+				else if (j == 0 || j == n - 1)
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
