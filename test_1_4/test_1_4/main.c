ef0a079117a3746e3e94265a45b88548b2da0a10
子期 <122613859+chengwya@users.noreply.github.com>
2024-01-04 22:12:47 +0800

    Add files via upload

---
+++ test_1_4/test_1_4/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "test.h"
+
+int main()
+{
+	print();
+	ret = add(3, 5);
+	printf("%d\n", ret);
+	return 0;
+}
---
+++ test_1_4/test_1_4/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "test.h"
+
+int ret = 0;
+
+void print()
+{
+	printf("Hello\n");
+}
+
+int add(int x, int y)
+{
+	return x + y;
+}
