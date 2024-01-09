b81989f1896828b5cbf00a9c8dfb5fc043bbc28f
子期 <122613859+chengwya@users.noreply.github.com>
2024-01-09 23:35:05 +0800

    Add files via upload

---
+++ test_1_9-2/test_1_9-2/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("Hello World!\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int d = 2;
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 1;
+//	printf("a = %d b = %d c = %d d = %d\n", a, b, c, d);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr[3][3] = {0,1,2,3,4,5,6,7,8};
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x, &y);
+//	int z = 0;
+//	int z = x;
+//	int x = y;
+//	int y = z;
+//	printf("x = %d y = %d\n", x, y);
+//	return 0;
+//}
+
