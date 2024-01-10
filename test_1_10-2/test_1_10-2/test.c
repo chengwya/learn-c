08749660921f8a4a0a21f970ffd0ad4c5a982509
子期 <122613859+chengwya@users.noreply.github.com>
2024-01-10 23:28:43 +0800

    Add files via upload

---
+++ test_1_10-2/test_1_10-2/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//
+//{
+//	printf("%d\n", sizeof(char));		//1
+//	printf("%d\n", sizeof(short));		//2
+//	printf("%d\n", sizeof(int));		//4
+//	printf("%d\n", sizeof(long));		//4
+//	printf("%d\n", sizeof(long long));  //8
+//	printf("%d\n", sizeof(float));		//4
+//	printf("%d\n", sizeof(double));		//8
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", sizeof (a));  //1
+//	printf("%d\n", sizeof(int)); //2
+//	printf("%d\n", sizeof a);    //3
+//	printf("%d\n", sizeof int);  //4
+//	//1、2、3是正确的
+//	//4是错误的,int是关键字不能直接使用sizeof关键字
+//	//sizeof不是函数，它是关键字或是操作符
+//	return 0;
+//}
+
+#include <stdio.h>
+
+#define INT_MAX;
+
+int Add_Init()
+{
+	;
+}
+
+int main()
+{
+	int g_val = 0;
+	int iMax = 0;
+	int Add_Init();
+	int Contav = 0;
+	int CON = 0;
+	return 0;
+}
